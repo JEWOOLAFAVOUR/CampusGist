f7e02e41523c444c339e1ebf9e7cfcc84af43558
John Doe <ibironkejohnsonade@gmail.com>
2023-03-10 02:30:33 -0800

    10/03/2023 first commit

--- constants/images.js
+++ constants/images.js
@@ -2,6 +2,14 @@ export const image1 = require('../assets/images/image1.jpg');
 export const image2 = require('../assets/images/image2.jpg');
 export const image5 = require('../assets/images/image5.jpg');
 export const image6 = require('../assets/images/image6.jpg');
+export const pic1 = require('../assets/images/pic1.jpg');
+export const pic2 = require('../assets/images/pic2.jpg');
+export const pic3 = require('../assets/images/pic3.jpg');
+export const pic4 = require('../assets/images/pic4.jpg');
+export const pic5 = require('../assets/images/pic5.jpg');
+export const pic6 = require('../assets/images/pic6.jpg');
+export const pic7 = require('../assets/images/pic7.jpg');
+export const pic8 = require('../assets/images/pic8.png');
 export const profile2 = require('../assets/images/profile2.jpg');
 export const profile3 = require('../assets/images/profile3.jpg');
 export const profile4 = require('../assets/images/profile4.jpg');
@@ -21,6 +29,14 @@ export default {
     image2,
     image5,
     image6,
+    pic1,
+    pic2,
+    pic3,
+    pic4,
+    pic5,
+    pic6,
+    pic7,
+    pic8,
     profile2,
     profile3,
     profile4,
