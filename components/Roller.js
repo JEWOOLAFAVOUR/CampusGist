1db8cff6375f1afcb342a825e96f69f63fd24830
John Doe <ibironkejohnsonade@gmail.com>
2023-03-15 04:37:15 -0700

    k

--- components/Roller.js
+++ components/Roller.js
@@ -26,7 +26,12 @@ export default Roller;
 const styles = StyleSheet.create({});
 
 
+// I'm having an app by a client last night to check, it about redesigning/cloning the app, the beta version already done about to be release for the beta testing, but the client developer wasn't able to aceive Linear Accelerometer using Flutter, so he said I should clone/redesign the app with React-Native.
+// The app is about "accurate and estimated vital sign measurement with your phone"
 
+// Blood Pressure, Breath Count, Step Count, Speed Measurement, Noise Measurement
+
+// so he said I should write a quotation for it, note i'm using react native in clonning/redsegin the app all over from scarath from flutter, backend is ready, because they
 // import { StyleSheet, Text, View } from 'react-native'
 // import React from 'react'
 // import Spinner from 'react-native-loading-spinner-overlay';
